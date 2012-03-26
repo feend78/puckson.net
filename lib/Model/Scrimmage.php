780b82ca8da616abc97400f56e9d871898796582
Dan Feeney <feend78@gmail.com>
2012-03-26 11:14:33 -0500

    What a morass! rolling back an incorrect roll-back Revert "Revert "signups working""
    This reverts commit 34a8c52c31558e262893237fdf5ec98b48d2ecf5.

--- lib/Model/Scrimmage.php
+++ lib/Model/Scrimmage.php
@@ -7,13 +7,39 @@ class Model_Scrimmage extends Model_Table {
     function init(){
         parent::init();
 
-        $this->addField('date_time')->type('datetime');
-        $this->addField('location');
+        $this->addField('date')->type('date')->mandatory(true);
+        $this->addField('start_time')->type('datetime')->mandatory(true);
+        $this->addField('location')->mandatory(true);
+
+        $this->addField('current_start')->type('timestamp');
+        $this->addField('current_end')->type('timestamp');
 
         $this->addField('created_dts')->type('timestamp')->system(true);
         $this->addField('updated_dts')->type('timestamp')->system(true);
     }
-    function sign_up($player_id) {
-        $m = $this->add(Model_Signup);
+    function beforeModify(&$data) {
+        parent::beforeModify($data);
+
+        $data['current_start'] = $mbr_types[$this->getField('type')].' Membership';
+    }
+    function signPlayerUp($player_id, $position) {        
+        $p=$this->add('Model_Player')->loadData($player_id);
+        $s=$this->add('Model_Signup')->addCondition('player_id', $player_id);
+
+        $s->loadBy('scrimmage_id',    $this->get('id'));
+
+        $s->set('scrimmage_id',       $this->get('id'))
+          ->set('player_id',          $p->get('id'))
+          ->set('position',           $position)
+          ->set('priority',           $p->get('priority'))
+          ->set('signup_dts',         date('Y-m-d H:i:s'))
+          ->update()
+          ;
+    }
+    function cancelSignup($player_id) {
+        $s=$this->add('Model_Signup')
+          ->addCondition('player_id', $player_id)
+          ->loadBy('scrimmage_id',    $this->get('id'))
+          ->delete();
     }
 }
